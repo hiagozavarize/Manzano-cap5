03fdce2bc7aa46a4d70039cc82ccb17a8226c7eb
Hiago <78562407+hiagozavarizex2@users.noreply.github.com>
2023-07-12 19:52:57 -0300

    ajuste de calculo de fatorial

--- exeO.c
+++ exeO.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
 int main() {
-  float fat = 1;
   int num;
   
-  for(int i = 1; i <= 10; i++){
-    if(i % 2 != 0){
-      for(num = i; num >= 1; num--){
-        fat *= num;
-      }
-      printf("o fatorial de %d é %.1f\n", i, fat);
-    }else{
-      continue;
+  for(int i = 1; i <= 10; i += 2){
+    int fat = 1;
+    for(num = i; num >= 1; num--){
+      fat *= num;
     }
+    printf("o fatorial de %d é %d\n", i, fat);
   }
   return 0;
 }
